f185f4c150c29dd4783091498e26ad1994fbb03f
Neyo <naledihloni14@gmail.com>
2025-12-13 22:10:25 +0200

    Update urls.py

--- task/manager/manager/urls.py
+++ task/manager/manager/urls.py
@@ -23,3 +23,16 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('tasks_manager.urls')),
 ]
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.task_list, name='task_list'),
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
+    path('task/create/', views.task_create, name='task_create'),
+    path('task/<int:pk>/edit/', views.task_update, name='task_update'),
+    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
+]
+
